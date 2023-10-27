6884bd3130ab6cecee79c96fa1a06099b8bb035e
subashn2003 <144302646+subashn2003@users.noreply.github.com>
2023-10-27 12:11:18 +0530

    Add files via upload

--- main (2).py
+++ main (2).py
@@ -1,37 +1,12 @@
 #
-class BankAccount:
+def linear_search_product(product_list, target_product):
+    indices = []
+    for i, product in enumerate(product_list):
+        if product == target_product:
+            indices.append(i)
+    return indices
 
-  def __init__(self, account_number, account_holder_name, initial_balance=0):
-    self.__account_number = account_number
-    self.__account_holder_name = account_holder_name
-    self.__account_balance = initial_balance
-
-  def deposit(self, amount):
-    if amount > 0:
-      self.__account_balance += amount
-      return f"Deposited ₹{amount}. New balance: ₹{self.__account_balance}"
-    else:
-      return "Invalid deposit amount. Please enter a positive amount."
-
-  def withdraw(self, amount):
-    if 0 < amount <= self.__account_balance:
-      self.__account_balance -= amount
-      return f"Withdrew ₹{amount}. New balance: ₹{self.__account_balance}"
-    elif amount > self.__account_balance:
-      return "Insufficient funds."
-    else:
-      return "Invalid withdrawal amount. Please enter a positive amount."
-
-  def display_balance(self):
-    return f"Account balance for {self.__account_holder_name}: ₹{self.__account_balance}"
-
-
-# Testing the BankAccount class
-if __name__ == "__main__":
-  account1 = BankAccount("123456789", "Ganeshkumar", 1000)
-
-  print(account1.display_balance())
-  print(account1.deposit(500))
-  print(account1.withdraw(200))
-  print(account1.withdraw(1500))
-  print(account1.display_balance())
+products = ["Apple", "Banana", "Apple", "Orange", "Pear", "Apple"]
+target = "Apple"
+result = linear_search_product(products, target)
+print(result)
